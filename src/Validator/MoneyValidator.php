6c7068d5087e0e41b07a7df327183236c56ea7b5
George Steel <george@net-glue.co.uk>
2019-12-16 15:03:15 +0000

    Add validator to validate composite array input

---
+++ src/Validator/MoneyValidator.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+namespace ACE\Money\Validator;
+
+use Money\Money;
+use Zend\Validator\AbstractValidator;
+use function is_array;
+use function is_numeric;
+
+class MoneyValidator extends AbstractValidator
+{
+    public const NOT_ARRAY = 'inputNotArray';
+    public const MISSING_KEYS = 'missingArrayKeys';
+    public const INVALID_CURRENCY = 'invalidCurrency';
+    public const NOT_NUMERIC = 'notANumber';
+
+    /** @var CurrencyValidator */
+    private $currencyValidator;
+
+    /** @var string[] */
+    protected $messageTemplates = [
+        self::NOT_ARRAY => 'Expected the input to be an array',
+        self::MISSING_KEYS => 'The input array should contain keys for "amount" and "currency"',
+        self::INVALID_CURRENCY => 'You provided an invalid currency code',
+        self::NOT_NUMERIC => 'The amount value should be numeric',
+    ];
+
+    public function __construct(CurrencyValidator $currencyValidator)
+    {
+        $this->currencyValidator = $currencyValidator;
+        parent::__construct();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isValid($value) : bool
+    {
+        if ($value instanceof Money) {
+            return true;
+        }
+        if (! is_array($value)) {
+            $this->error(self::NOT_ARRAY);
+            return false;
+        }
+        if (! isset($value['amount'], $value['currency'])) {
+            $this->error(self::MISSING_KEYS);
+            return false;
+        }
+        if (! $this->currencyValidator->isValid($value['currency'])) {
+            $this->error(self::INVALID_CURRENCY);
+            return false;
+        }
+        if (! is_numeric($value['amount'])) {
+            $this->error(self::NOT_NUMERIC);
+            return false;
+        }
+        return true;
+    }
+}
---
+++ test/Money/Validator/MoneyValidatorTest.php
@@ -0,0 +1,98 @@
+<?php
+declare(strict_types=1);
+
+namespace ACETest\Money\Validator;
+
+use ACE\Money\Filter\ToMoney;
+use ACE\Money\Validator\CurrencyValidator;
+use ACE\Money\Validator\MoneyValidator;
+use Money\Currencies\ISOCurrencies;
+use Money\Currency;
+use Money\Money;
+use PHPUnit\Framework\TestCase;
+
+class MoneyValidatorTest extends TestCase
+{
+    /** @var MoneyValidator */
+    private $validator;
+
+    protected function setUp() : void
+    {
+        parent::setUp();
+        $this->validator = new MoneyValidator(
+            new CurrencyValidator(
+                new ISOCurrencies()
+            )
+        );
+    }
+
+    /** @return mixed[] */
+    public function validValues() : array
+    {
+        return [
+            ['1.00', 'GBP', 100, 'GBP'],
+            [1.10, 'GBP', 110, 'GBP'],
+            ['123', 'GBP', 12300, 'GBP'],
+            ['0', 'GBP', 0, 'GBP'],
+            ['0.0', 'GBP', 0, 'GBP'],
+            [0, 'GBP', 0, 'GBP'],
+            [0.0, 'GBP', 0, 'GBP'],
+            ['-1.00', 'GBP', -100, 'GBP'],
+            [-1, 'GBP', -100, 'GBP'],
+        ];
+    }
+
+    /** @return mixed[] */
+    public function invalidInput() : array
+    {
+        return [
+            [[]],
+            ['foo'],
+            [1],
+            [null],
+            [1.9],
+            [['currency' => '', 'amount' => '']],
+            [['currency' => 'GBP', 'amount' => '']],
+            [['currency' => 'GBP', 'amount' => null]],
+            [['currency' => '123', 'amount' => 'GBP']],
+            [['currency' => '123', 'amount' => 'GBP']],
+        ];
+    }
+
+    /**
+     * @param mixed $inputAmount
+     * @param mixed $inputCode
+     *
+     * @dataProvider validValues
+     */
+    public function testValidValues($inputAmount, $inputCode, int $expectAmount, string $expectCode) : void
+    {
+        $input = [
+            'currency' => $inputCode,
+            'amount' => $inputAmount,
+        ];
+        $this->assertTrue(
+            $this->validator->isValid($input)
+        );
+    }
+
+    /**
+     * @param mixed $input
+     *
+     * @dataProvider invalidInput
+     */
+    public function testInvalidInput($input) : void
+    {
+        $this->assertFalse(
+            $this->validator->isValid($input)
+        );
+    }
+
+    public function testMoneyInstanceWillBeValid() : void
+    {
+        $input = new Money(100, new Currency('GBP'));
+        $this->assertTrue(
+            $this->validator->isValid($input)
+        );
+    }
+}
